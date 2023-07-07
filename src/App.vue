9a62f4cc7e4faef2226338ddd4fa03bc311103d1
Kwok Yew Weng <66321555+kywleon@users.noreply.github.com>
2023-07-07 23:17:32 +0800

    Learn Vue JS 3 Tutorial - 50 - Reusability with Mixins

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,18 @@
 <template>
   <div>
-    
+    <ClickCounter />
+    <HoverCounter />
   </div>
 </template>
 
 <script>
-
+import ClickCounter from './components/ClickCounter.vue'
+import HoverCounter from './components/HoverCounter.vue'
 export default {
   name: 'App',
   components: {
-    
+    ClickCounter,
+    HoverCounter
   }
 }
 </script>
