964f3ac12c4f496c33b56750439827573e1cf7e6
Kwok Yew Weng <66321555+kywleon@users.noreply.github.com>
2023-07-07 18:18:58 +0800

    Learn Vue JS 3 Tutorial - 47 - Lifecycle Hooks

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,16 @@
 <template>
   <div>
-    
+      <Parent />
   </div>
 </template>
 
 <script>
+import Parent from './components/Parent.vue'
 
 export default {
   name: 'App',
   components: {
-    
+    Parent
   }
 }
 </script>
