cc06a50b9d3ac989d86f6a9c29360fa1c766bf62
Kwok Yew Weng <66321555+kywleon@users.noreply.github.com>
2023-07-09 18:01:36 +0800

    Learn Vue JS 3 Tutorial - 62 - Template Refs and Composition API

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,15 @@
 <template>
   <div>
-    
+    <TemplateRef />
   </div>
 </template>
 
 <script>
-
+import TemplateRef from './components/TemplateRef.vue'
 export default {
   name: 'App',
   components: {
-    
+    TemplateRef
   }
 }
 </script>
