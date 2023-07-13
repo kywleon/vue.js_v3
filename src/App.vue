a3c0ab8d66583d15786e06e3dc9f447e440f2e5b
Kwok Yew Weng <66321555+kywleon@users.noreply.github.com>
2023-07-13 21:50:25 +0800

    Learn Vue JS 3 Tutorial - 65 - Reusability with Composition API

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,19 @@
 <template>
   <div>
-    
+    <ClickCounter/>
+    <HoverCounter/>
   </div>
 </template>
 
 <script>
+import ClickCounter from './components/ClickCounter.vue'
+import HoverCounter from './components/HoverCounter.vue'
 
 export default {
   name: 'App',
   components: {
-    
+    ClickCounter,
+    HoverCounter
   }
 }
 </script>
